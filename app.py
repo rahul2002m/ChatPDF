ca7cd0e9655c5682dce95a5082eaeaa8c239f1fa
rahul2002m <rahul2002m@gmail.com>
2023-09-03 18:47:25 +0530

    extract text from file and split into chunks

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from PyPDF2 import PdfReader
+
+
+def get_text(docs):
+    doc_text = ""
+    for doc in docs:
+        pdf_reader = PdfReader(doc)
+        for each_page in pdf_reader.pages:
+            doc_text += each_page.extractText()
+        doc_text += "\n"
+
+    return doc_text
+
+
+def get_chunks(data):
+    text_splitter = CharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=250, length_function=len
+    )
+    text_chunks = text_splitter.split_text(data)
+    return text_chunks
 
 
 def main():
@@ -17,7 +38,9 @@ def main():
 
         st.session_state.doc_len = len(docs)
         with st.spinner("Processing..."):
-            pass
+            doc_text = get_text(docs)
+            doc_chunks = get_chunks(doc_text)
+            st.write(doc_chunks)
 
 
 if __name__ == "__main__":
